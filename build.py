f2ec64d5ef67bcc795eba6b0093745f136b44f16
Adam Johnson <me@adamj.eu>
2026-01-23 22:56:09 +0000

    Add headerpad_max_install_names flag on macOS builds
    Attempting to fix delocate crash seen in https://github.com/adamchainz/icu4py/pull/45:
    
    ```
    stderr:error: /Applications/Xcode_16.4.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/install_name_tool: changing install names or rpaths can't be redone for: /private/var/folders/kg/7q73ww8s3llgyl61c9z_j5g40000gn/T/tmpf95_c4og/wheel/icu4py/.dylibs/libicui18n.78.2.dylib (for architecture arm64) because larger updated load commands do not fit (the program must be relinked, and you may need to use -headerpad or -headerpad_max_install_names)
    ```
    
    Using headerpad in icu4py itself is not enough as the dylibs reference each other too, so need padded headers.

--- build.py
+++ build.py
@@ -121,6 +121,8 @@ def build_unix(source_dir: Path, install_dir: Path, platform_name: str) -> None:
         "--disable-tests",
         "CPPFLAGS=-DU_CHARSET_IS_UTF8=1",
     ]
+    if platform_name == "macos":
+        configure_args.append("LDFLAGS=-Wl,-headerpad_max_install_names")
 
     run(configure_args, cwd=source_dir)
 
